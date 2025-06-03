bdf486c1ed63d57d269bab89125a953b23420dce
moonashaikh <160643434+moonashaikh@users.noreply.github.com>
2025-06-03 20:05:15 +0500

    Add files via upload

---
+++ chapter-1/main.py
@@ -0,0 +1,14 @@
+# def main():
+#     print("Hello from chapter-1!")
+
+
+# if __name__ == "__main__":
+#     main()
+   
+    
+def main():
+    print("Hello")
+
+if __name__ == "__main__":
+    main()
+    print("Pakistan zinda bad")
